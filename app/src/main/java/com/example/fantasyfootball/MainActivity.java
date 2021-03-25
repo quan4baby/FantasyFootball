0b14b825204d71680be1b3ba5ccc2727c2478f85
Shaquan Patterson <PattersonS3@chc.edu>
2021-03-25 11:39:03 -0400

    Thursday, March 25

--- app/src/main/java/com/example/fantasyfootball/MainActivity.java
+++ app/src/main/java/com/example/fantasyfootball/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     // declare DBHandler
     DBHandler dbHandler;
@@ -28,8 +28,10 @@ public class MainActivity extends AppCompatActivity {
     Spinner teamSpinner;
 
     // declare Strings to store year and major selected in Spinners
+
     String position;
     String team;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        position = "";
+        team = "";
 
         // initialize DBHandler
         dbHandler = new DBHandler(this, null);
@@ -56,70 +61,93 @@ public class MainActivity extends AppCompatActivity {
                 R.array.team, android.R.layout.simple_spinner_item);
 
         // further stylize ArrayAdapters with style defined by simple_spinner_dropdown_item
-       positionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-      teamAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        positionAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        teamAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
 
         // set ArrayAdapters on Spinners
         positionSpinner.setAdapter(positionAdapter);
         teamSpinner.setAdapter(teamAdapter);
 
+        // register On Item Selected Listener to Spinners
+        positionSpinner.setOnItemSelectedListener(this);
+        teamSpinner.setOnItemSelectedListener(this);
 
+    }
+
+    /**
+     * This method further initializes the Action Bar of the activity.
+     * It gets the code (XML) in the menu resource file and incorporates it
+     * into the Action Bar.
+     *
+     * @param menu menu resource file for the activity
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    /**
+     * This method gets called when a menu-item in the overflow menu is selected.
+     *
+     * @param menuItem selected team
+     * @return String that contains count of the number of students who have the selected major.
+     */
+    public void getMessage(MenuItem menuItem) {
+        int count = dbHandler.getCount("Eagles");
+        int count2 = dbHandler.getCount("Steelers");
+        int count3 = dbHandler.getCount("Cowboys");
+
+        Toast.makeText(this, "Eagles: " + count + "\nSteelers: " + count2 + "\nCowboys: " + count3, Toast.LENGTH_LONG).show();
+
+    }
+
+    /**
+     * This method gets called when the add button in the Action Bar gets clicked.
+     *
+     * @param menuItem add student menu item
+     */
+    public void add(MenuItem menuItem) {
+        // get data input in EditText and store it in String
+        String name = nameEditText.getText().toString();
+
+        // trim Strings and see if they're equal to empty Strings
+        if (name.trim().equals("") || position.trim().equals("") || team.trim().equals("")) {
+            // display "Please enter a name, store, and date!" Toast if any of the Strings are empty
+            Toast.makeText(this, "Please enter a position, name, and team!", Toast.LENGTH_LONG).show();
+        } else {
+            // add item into database
+            dbHandler.add(name, position, team);
+
+            // display "Student added!" Toast of none of the Strings are empty
+            Toast.makeText(this, "Player added!", Toast.LENGTH_LONG).show();
+        }
     }
 
 
 
 
-        /**
-         * This method gets called when a menu-item in the overflow menu is selected.
-         * @param team selected team
-         * @return String that contains count of the number of students who have the selected major.
-         */
-        public String getMessage (String team) {
-            int count = dbHandler.getCount(team);
-            return (count == 1 ? count + " player." : count + " players.");
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position1, long id) {
+        // get the id of the Spinner that called method
+        switch (parent.getId()) {
+            case R.id.teamSpinner:
+                // get the item selected in the Spinner and store it in String
+                team = parent.getItemAtPosition(position1).toString();
+                break;
+            case R.id.positionSpinner:
+                // get the item selected in the Spinner and store it in String
+                position = parent.getItemAtPosition(position1).toString();
+                break;
         }
 
-        /**
-         * This method gets called when the add button in the Action Bar gets clicked.
-         * @param menuItem add student menu item
-         */
-        public void add(MenuItem menuItem) {
-            // get data input in EditText and store it in String
-            String name = nameEditText.getText().toString();
+    }
 
-            // trim Strings and see if they're equal to empty Strings
-            if (name.trim().equals("") || position.trim().equals("") || team.trim().equals("")){
-                // display "Please enter a name, store, and date!" Toast if any of the Strings are empty
-                Toast.makeText(this, "Please enter a position, name, and team!", Toast.LENGTH_LONG).show();
-            } else {
-                // add item into database
-                dbHandler.add(name, position, team);
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
-                // display "Student added!" Toast of none of the Strings are empty
-                Toast.makeText(this, "Player added!", Toast.LENGTH_LONG).show();
-            }
-        }
-
-        /**
-         * This method gets called when an item in one of the Spinners is selected.
-         * @param parent Spinner AdapterView
-         * @param view MainActivity view
-         * @param position position of item in Spinner that was selected
-         * @param id database id of item in Spinner that was selected
-         */
-        public void onItemSelected(AdapterView<?> parent, View view, String position, long id) {
-            // get the id of the Spinner that called method
-            switch (parent.getId()) {
-                case R.id.positionSpinner:
-                    // get the item selected in the Spinner and store it in String
-                  position = parent.getItemAtPosition(Integer.parseInt(position)).toString();
-                    break;
-                case R.id.teamSpinner:
-                    // get the item selected in the Spinner and store it in String
-                    team = parent.getItemAtPosition(Integer.parseInt(position)).toString();
-                    break;
-            }
-        }
-
-
+    }
 }
